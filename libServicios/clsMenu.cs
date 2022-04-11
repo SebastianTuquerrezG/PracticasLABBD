4f9e546dfb64d7deeba304677377b1e506ad09ec
Joan Sebastian Tuquerrez Gomez <joangomezsebas@gmail.com>
2022-04-11 15:59:21 -0500

    Update clsMenu.cs

--- libServicios/clsMenu.cs
+++ libServicios/clsMenu.cs
@@ -5,8 +5,8 @@ namespace Servicios.Menus
     public class clsMenu
     {
         #region Atributos
-        protected string FileToRead = @"C:\Users\Personal\OneDrive - unicauca.edu.co\Documentos\Semestre 4\Practica1\positions.csv";
-        protected string FileToRewritten = @"C:\Users\Personal\OneDrive - unicauca.edu.co\Documentos\Semestre 4\BD1\ej.txt";
+        protected string FileToRead = @"C:\MyFilepositions.csv";
+        protected string FileToRewritten = @"C:\MyFileej.txt";
         protected int atrOpcion = -1;
         protected int atrOpcionSalir;
         protected string[] atrItems; 
